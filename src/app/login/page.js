49881ae14db911d8cd85577596ca36a1d328449f
Leo Zheng <leozhengcs@gmail.com>
2025-03-11 14:20:01 -0700

    Add tailwind components

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,21 +1,11 @@
 // pages/login.js
 import Layout from '../components/Layout';
+import Login from '../components/Login';
 
-export default function Login() {
+export default function LoginPage() {
   return (
     <Layout>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      <Login/>
     </Layout>
   );
 }
