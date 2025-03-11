6ff61795999e3a7b284de2233d5ffc44246daa29
Leo Zheng <leozhengcs@gmail.com>
2025-03-11 14:09:13 -0700

    Add tailwindcss

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Layout from './components/Layout';
 export default function Home() {
   return (
     <Layout>
-      <h1>Welcome to the Home Page</h1>
+      <h1 className='font-bold'>Welcome to the Home Page</h1>
       <p>This is the main landing page of your application.</p>
     </Layout>
   );
