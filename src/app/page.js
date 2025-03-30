e6461acf6b4155e5e5392e5c9fe5d1fc903c96e1
Leo Zheng <leozhengcs@gmail.com>
2025-03-29 17:33:20 -0700

    Add home auth redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,45 @@
 'use client';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Layout from './components/Layout';
 
 export default function Home() {
+  const router = useRouter();
   const [numCalls, setNumCalls] = useState(null);
-    useEffect(() => {
-        const email = sessionStorage.getItem("email")
-        const session = sessionStorage.getItem("sessionToken")
-    
-        const getCalls = async () =>{
-            try{
-                const response = await fetch("/api/dashboard", {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({email, session}),
-                })
+  const [isLoading, setIsLoading] = useState(true); // new state to block render
 
-                // TODO Reroute
-                if (!response.ok) throw new Error("Failed to fetch data");
+  useEffect(() => {
+      const email = sessionStorage.getItem("email")
+      const session = sessionStorage.getItem("sessionToken")
+  
+      const getCalls = async () =>{
+          try{
+            const response = await fetch("/api/dashboard", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({email, session}),
+            })
 
-                const data = await response.json();
-                console.log(data);
-                setNumCalls(data.numcalls);
+            // TODO Reroute
+            if (!response.ok) {
+              router.push('/login');
+              return;
+            };
 
-            } catch(error){
-                console.log(error);
-            }
-        }
-        getCalls();
-  }, []);
+            const data = await response.json();
+            console.log(data);
+            setNumCalls(data.numcalls);
+            setIsLoading(false);
+          } catch(error){
+            console.log(error);
+          }
+      }
+      getCalls();
+  }, [router]);
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
     <Layout>
