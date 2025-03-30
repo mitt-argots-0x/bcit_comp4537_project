4f70387809425f19de99d9de008dfb9823cf8853
Donghoon Oh <mitt.argots.0x@icloud.com>
2025-03-29 19:26:09 -0700

    change static to dynamic generation

--- src/app/reset-password/page.js
+++ src/app/reset-password/page.js
@@ -1,6 +1,8 @@
 import Layout from '../components/Layout';
 import ResetPassword from '../components/ResetPassword';
 
+export const dynamic = 'force-dynamic';
+
 export default function ResetPasswordPage() {
   return (
     <Layout>
